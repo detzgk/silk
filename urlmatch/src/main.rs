874dfddb7910f2b7be3428b694eb0025f736e26e
Eli Green <eli@zigr.org>
2018-04-01 02:57:18 +0200

    add UrlParser trait

--- urlmatch/src/main.rs
+++ urlmatch/src/main.rs
@@ -1,31 +1,44 @@
+use std::iter::Peekable;
+use std::str::Chars;
+
+trait UrlParser {
+    fn parse_from_url(Peekable<Chars>) -> Self;
+}
+
+impl UrlParser for i32 {
+    fn parse_from_url(iter : Peekable<Chars>) -> i32 {
+        42
+    }
+}
+
 macro_rules! urlmatch {
-    ($ty:expr, $( $verb:ident ( $( $url:tt )+ ) => $body:expr )+) => (
-        let url_iter = $ty.chars().peekable();
+    ($iter:expr, $( $verb:ident ( $( $url:tt )+ ) => $body:expr )+) => (
+        let url_iter = $iter.chars().peekable();
         println!("Oh hi: {:?}", url_iter);
         $(
             println!("Verb: {:?}", $verb);
-            urlchunks!($($url)+);
+            urlchunks!(url_iter, $($url)+);
             println!("Body: {:?}", $body);
         )+
     )
 }
 
 macro_rules! urlchunks {
-    ($first:tt, $( $rest:tt )+) => (
-        println!("{:?}", $first);
-        urlchunks!($($rest)+);
-    );
-    ($first:tt) => (
+    ($iter:ident, $first:tt) => (
         println!("{:?}", $first);
     );
-    ($first:ident : $ty:ty) => (
-        let $first : $ty = Default::default();
+    ($iter:ident, $first:tt, $( $rest:tt )+) => (
+        println!("{:?}", $first);
+        urlchunks!($iter, $($rest)+);
+    );
+    ($iter:ident, $first:ident : $ty:ty) => (
+        let $first : $ty = UrlParser::parse_from_url($iter);
         println!("{:?} <== {:?}", stringify!($first), stringify!($ty));
     );
-    ($first:ident : $ty:ty, $( $rest:tt )+) => (
-        let $first : $ty = Default::default();
+    ($iter:ident, $first:ident : $ty:ty, $( $rest:tt )+) => (
+        let $first : $ty = UrlParser::parse_from_url($iter);
         println!("{:?}", stringify!($first));
-        urlchunks!($($rest)+);
+        urlchunks!($iter, $($rest)+);
     )
 }
 
