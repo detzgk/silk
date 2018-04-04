e77ce8110a1750b2f0b606dbe45d156f7f51e1f2
Eli Green <eli@zigr.org>
2018-04-04 18:37:13 +0200

    Implement url parsing for all integral types

--- urlmatch/src/main.rs
+++ urlmatch/src/main.rs
@@ -1,15 +1,50 @@
 use std::iter::Peekable;
 use std::str::Chars;
+use std::u32;
 
 pub trait UrlParser: Sized {
     fn parse_from_url(&mut Peekable<Chars>) -> Option<Self>;
 }
 
-impl UrlParser for i32 {
-    fn parse_from_url(iter : &mut Peekable<Chars>) -> Option<i32> {
-        Some(42)
+macro_rules! impl_num_parser {
+    ($num:ty) => {
+        impl UrlParser for $num {
+            fn parse_from_url(iter : &mut Peekable<Chars>) -> Option<$num> {
+                let mut consumed = 0 as $num;
+                let mut result = 0 as $num;
+                for ch in iter {
+                    consumed += 1;
+                    let x = match ch.to_digit(10) {
+                        Some(x) => x as $num,
+                        None => return None,
+                    };
+                    result = match result.checked_mul(10) {
+                        Some(result) => result,
+                        None => return None,
+                    };
+                    result = match result.checked_add(x) {
+                        Some(result) => result,
+                        None => return None,
+                    };
+                }
+                if consumed == 0 {
+                    None
+                } else {
+                    Some(result)
+                }
+            }
+        }
     }
 }
+
+impl_num_parser!(u8);
+impl_num_parser!(u16);
+impl_num_parser!(u32);
+impl_num_parser!(u64);
+impl_num_parser!(i8);
+impl_num_parser!(i16);
+impl_num_parser!(i32);
+impl_num_parser!(i64);
 
 fn matches(iter : &mut Peekable<Chars>, text: &'static str) -> bool {
     let mut other = text.chars();
